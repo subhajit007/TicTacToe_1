d3873458e6b71cd7c483b3016b1e04d90dfb180f
subhajit007 <subhajitusa007@gmail.com>
2015-04-03 11:18:57 -0700

    first commit

---
+++ test.js
@@ -0,0 +1,154 @@
+window.onload = function(){
+    var html5_audiotypes={ //defining audio file extensions and their associated audio types.
+    "mp3": "audio/mpeg",
+    }
+
+function createsoundbite(sound){
+
+    var html5audio=document.createElement('audio')
+
+    if (html5audio.canPlayType){ //check support for HTML5 audio
+
+        for (var i=0; i<arguments.length; i++){
+            var sourceel=document.createElement('source')
+            sourceel.setAttribute('src', arguments[i])
+
+            if (arguments[i].match(/\.(\w+)$/i))
+                sourceel.setAttribute('type', html5_audiotypes[RegExp.$1])
+            html5audio.appendChild(sourceel)
+        }
+
+        html5audio.load()
+        html5audio.playclip=function(){
+            html5audio.pause()
+            html5audio.currentTime=0
+            html5audio.play()
+        }
+        return html5audio
+    }
+    else{
+        return {playclip:function(){throw new Error("Your browser doesn't support HTML5 audio unfortunately")}}
+    }
+}
+
+// sound clips
+
+var coins=createsoundbite( "CLICKTIC.mp3")
+console.log(document.getElementsByClassName('btn'));
+    var btn = document.getElementsByClassName('btn');
+
+    for(i=0; i < btn.length; i++)
+    {
+        console.log(btn[i]);
+        btn[i].addEventListener('click', function(){
+            console.log('clickedButton');
+            coins.playclip();
+        });
+    }
+
+}
+
+// creating palyer and function which will take disabled from btn id and the game logic
+var player = 1;
+
+function clickBtn(btn){
+    if (player==1)
+    {
+    document.getElementById(btn).value= "X";
+    document.getElementById(btn).disabled ="disabled";
+    player +=1;
+    winner();
+    } else
+            {
+    document.getElementById(btn).value= "0";
+    document.getElementById(btn).disabled="disabled";
+    player -=1;
+    winner();
+                    }
+}
+
+function winner(){
+    if (
+        document.getElementById("btn1").value =="X" &&
+        document.getElementById("btn2").value =="X" &&
+        document.getElementById("btn3").value =="X" ||
+        document.getElementById("btn4").value =="X" &&
+        document.getElementById("btn5").value =="X" &&
+        document.getElementById("btn6").value =="X" ||
+        document.getElementById("btn7").value =="X" &&
+        document.getElementById("btn8").value =="X" &&
+        document.getElementById("btn9").value =="X" ||
+        document.getElementById("btn1").value =="X" &&
+        document.getElementById("btn4").value =="X" &&
+        document.getElementById("btn7").value =="X" ||
+        document.getElementById("btn2").value =="X" &&
+        document.getElementById("btn5").value =="X" &&
+        document.getElementById("btn8").value =="X" ||
+        document.getElementById("btn3").value =="X" &&
+        document.getElementById("btn6").value =="X" &&
+        document.getElementById("btn9").value =="X" ||
+        document.getElementById("btn1").value =="X" &&
+        document.getElementById("btn5").value =="X" &&
+        document.getElementById("btn9").value =="X" ||
+        document.getElementById("btn3").value =="X" &&
+        document.getElementById("btn5").value =="X" &&
+        document.getElementById("btn7").value =="X"
+){
+        document.getElementById('xyz').play(); //one sound clip is playing when alert triggers
+        alert("X you are the winner Your account has been Credited ");
+        reset(); //game is going to reset for a new session
+
+    } else if(
+        document.getElementById("btn1").value =="O" &&
+        document.getElementById("btn2").value =="O" &&
+        document.getElementById("btn3").value =="O" ||
+        document.getElementById("btn4").value =="O" &&
+        document.getElementById("btn5").value =="O" &&
+        document.getElementById("btn6").value =="O" ||
+        document.getElementById("btn7").value =="O" &&
+        document.getElementById("btn8").value =="O" &&
+        document.getElementById("btn9").value =="O" ||
+        document.getElementById("btn1").value =="O" &&
+        document.getElementById("btn4").value =="O" &&
+        document.getElementById("btn7").value =="O" ||
+        document.getElementById("btn2").value =="O" &&
+        document.getElementById("btn5").value =="O" &&
+        document.getElementById("btn8").value =="O" ||
+        document.getElementById("btn3").value =="O" &&
+        document.getElementById("btn6").value =="O" &&
+        document.getElementById("btn9").value =="O" ||
+        document.getElementById("btn1").value =="O" &&
+        document.getElementById("btn5").value =="O" &&
+        document.getElementById("btn9").value =="O" ||
+        document.getElementById("btn3").value =="O" &&
+        document.getElementById("btn5").value =="O" &&
+        document.getElementById("btn7").value =="O"
+        )
+
+        {
+        document.getElementById('xyz').play(); //one sound clip is playing when alert triggers
+        alert("O you are the winner Your account has been Credited ");
+        reset(); //game is going to reset for a new session
+}
+}
+
+ function reset(){
+        document.getElementById("btn1").value = " ";
+        document.getElementById("btn2").value = " ";
+        document.getElementById("btn3").value = " ";
+        document.getElementById("btn4").value = " ";
+        document.getElementById("btn5").value = " ";
+        document.getElementById("btn6").value = " ";
+        document.getElementById("btn7").value = " ";
+        document.getElementById("btn8").value = " ";
+        document.getElementById("btn9").value = " ";
+        document.getElementById("btn1").disabled = " ";
+        document.getElementById("btn2").disabled = " ";
+        document.getElementById("btn3").disabled = " ";
+        document.getElementById("btn4").disabled = " ";
+        document.getElementById("btn5").disabled = " ";
+        document.getElementById("btn6").disabled = " ";
+        document.getElementById("btn7").disabled = " ";
+        document.getElementById("btn8").disabled = " ";
+        document.getElementById("btn9").disabled = " ";
+}
